a01d744d093d1b08ad150267e82af4a1fa586bbb
saitg <codersait@gmail.com>
2024-05-05 11:14:48 +0300

    Create Student Entity

---
+++ src/main/java/com/zyfera/studentdataapi/entitiy/Student.java
@@ -0,0 +1,86 @@
+package com.zyfera.studentdataapi.entitiy;
+
+import java.util.List;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ElementCollection
+    @CollectionTable(name = "student_grades")
+    private List<Grade> grades;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, List<Grade> grades) {
+        this.id = id;
+        this.name = name;
+        this.grades = grades;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Grade> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(List<Grade> grades) {
+        this.grades = grades;
+    }
+}
+
+@Embeddable
+class Grade {
+    private String code;
+    private int value;
+
+    public Grade() {
+    }
+
+    public Grade(String code, int value) {
+        this.code = code;
+        this.value = value;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
