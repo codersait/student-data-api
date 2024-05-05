b673a2def66b4d5e6aa5a785326bd72f178f7ebc
saitg <codersait@gmail.com>
2024-05-05 11:51:15 +0300

    Create Student Controller

---
+++ src/main/java/com/zyfera/studentdataapi/controller/StudentController.java
@@ -0,0 +1,26 @@
+package com.zyfera.studentdataapi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zyfera.studentdataapi.entity.Student;
+import com.zyfera.studentdataapi.service.StudentService;
+
+@RestController
+public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping("/api/v1/students")
+    public ResponseEntity<Student> create(@RequestBody Student student) {
+        Student createdStudent = studentService.createStudent(student);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdStudent);
+    }
+
+}
