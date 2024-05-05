e0f95d1d98ec23ed49b2683cf70a4139582d97dd
saitg <codersait@gmail.com>
2024-05-05 11:08:54 +0300

    create project with dependencies

---
+++ src/main/java/com/zyfera/studentdataapi/StudentDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.zyfera.studentdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDataApiApplication.class, args);
+	}
+
+}
